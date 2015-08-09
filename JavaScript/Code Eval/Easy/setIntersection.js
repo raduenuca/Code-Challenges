fd55789e7a6f2c9d5243c6326366be46003b6012
Radu Enucă <raduenuca@gmail.com>
2015-08-09 23:26:13 +0300

    Added Set Intersection

---
+++ JavaScript/Code Eval/Easy/setIntersection.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+
+var parseIntEx = function (str) {
+    return parseInt(str);
+};
+
+var isInArray = function (arr) {
+    return function (val) {
+        return arr.indexOf(val) !== -1;
+    }
+};
+
+var intersect = function (a, b) {
+    var isInSetB = isInArray(b);
+    return a.filter(isInSetB);
+};
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        var sets = line.split(";");
+        var setA = sets[0].split(",").map(parseIntEx);
+        var setB = sets[1].split(",").map(parseIntEx);
+
+        console.log(intersect(setA, setB).join(","));
+    }
+});
