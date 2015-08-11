bfb843adcc4667f07c59de5d1525c3397f1d7be9
Radu Enuca <raduenuca@gmail.com>
2015-08-11 16:35:53 +0000

    Added Find a Writer

--- Code Eval/Easy/JavaScript/findAWriter.js
+++ Code Eval/Easy/JavaScript/findAWriter.js
@@ -1,15 +1,23 @@
 var fs = require("fs");
 
+var parseIntEx = function(str) {
+    return parseInt(str);
+};
+
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
     if (line !== "") {
         var input = line.trim().split("|");
-        var writerCode = input[0].trim().split("");
-        var key = input[1].trim().split(" ");
-        
-        var writer = key.map(function(keyPart){
-           return writerCode[keyPart - 1];
-        }).join("");
-        
+        var writerCode = input[0].split("");
+        var key = input[1].trim().split(" ").map(parseIntEx);
+
+        // var writer = key.map(function(keyPart){
+        //   return writerCode[keyPart - 1];
+        // }).join("");
+
+        var writer = key.reduce(function(w, keyPart) {
+            return w + writerCode[keyPart - 1];
+        }, "");
+
         console.log(writer);
     }
 });
