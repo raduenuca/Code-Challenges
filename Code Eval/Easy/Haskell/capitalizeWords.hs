1dec2c175090c8e54d7ac6ad9abbee02c23eedbc
Radu Enuca <raduenuca@gmail.com>
2015-08-12 14:02:48 +0000

    Added Python variant to Capitalize Words

---
+++ Code Eval/Easy/Haskell/capitalizeWords.hs
@@ -0,0 +1,13 @@
+import System.Environment (getArgs)
+import Data.Char
+
+main = do
+	[inpFile] <- getArgs
+	input <- readFile inpFile
+	-- print your output to stdout
+	mapM_ putStrLn $ capitalizeWords (lines input)
+
+capitalizeWords :: String -> String
+capitalizeWords = unwords . map capitalizeWord . words
+  where capitalizeWord [] = []
+        capitalizeWord (c:cs) = toUpper c : cs
---
+++ Code Eval/Easy/Python/capitalizeWords.py
@@ -0,0 +1,7 @@
+import sys
+test_cases = open(sys.argv[1], 'r')
+
+for test in test_cases:
+    print(" ".join(word[0].upper() + word[1:] for word in test.split()))
+
+test_cases.close()
