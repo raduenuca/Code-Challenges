3856111b6e2aebf2c65109c10bd68e746108909a
Radu Enuca <raduenuca@gmail.com>
2015-08-12 17:39:23 +0000

    Added Swap Elements

---
+++ Code Eval/Easy/JavaScript/swapElements.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+var parseIntEx = function(value){
+    return +value;
+};
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
+    if (line !== "") {
+        var input = line.trim().split(":");
+        
+        var list = input[0].trim().split(" ").map(parseIntEx);
+        var swapList = input[1].trim().split(", ")
+            .map(function(elem){
+                return elem.split("-").map(parseIntEx);
+            });
+            
+        swapList.forEach(function(swap){
+           var temp = list[swap[0]];
+           list[swap[0]] = list[swap[1]];
+           list[swap[1]] = temp;
+        });
+  
+        console.log(list.join(" "));
+    }
+});
