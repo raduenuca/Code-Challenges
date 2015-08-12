7ec18bbe4d0dd89ee5e66e7e03430b5782673de1
Radu Enuca <raduenuca@gmail.com>
2015-08-12 16:33:02 +0000

    Added Haskell variant for Capitalize Words

--- Code Eval/Easy/Haskell/capitalizeWords.hs
+++ Code Eval/Easy/Haskell/capitalizeWords.hs
@@ -5,7 +5,7 @@ main = do
 	[inpFile] <- getArgs
 	input <- readFile inpFile
 	-- print your output to stdout
-	mapM_ putStrLn $ capitalizeWords (lines input)
+	mapM_ putStrLn $ map capitalizeWords (lines input)
 
 capitalizeWords :: String -> String
 capitalizeWords = unwords . map capitalizeWord . words
