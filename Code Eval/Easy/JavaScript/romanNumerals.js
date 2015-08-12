e46e9e7e9d078eccad3a61dcf86966d9e5feae9a
Radu Enuca <raduenuca@gmail.com>
2015-08-12 07:58:06 +0000

    Updated Roman Numerals

--- Code Eval/Easy/JavaScript/romanNumerals.js
+++ Code Eval/Easy/JavaScript/romanNumerals.js
@@ -5,64 +5,39 @@ var parseIntEx = function(value) {
 };
 
 var parseRoman = function(value) {
-    var romanNumerals = {
+    var romanNumeralsMap = {
         "1": "I",
-        "4": "IV",
         "5": "V",
-        "9": "IX",
         "10": "X",
-        "40": "XL",
         "50": "L",
-        "90": "XC",
         "100": "C",
-        "400": "CD",
         "500": "D",
-        "900": "CM",
         "1000": "M"
     };
 
     var digits = value.split("").map(parseIntEx);
 
-    var decomposed = digits.map(function(digit, index, array) {
-        var zeroes =Array(array.length - index).join("0");
-        var stringRepresentation = digit.toString() + zeroes;
-        
-        if(typeof romanNumerals[stringRepresentation] !== "undefined"){
-            return romanNumerals[stringRepresentation];
-        }
-        
-        var literal = romanNumerals["1" + zeroes];        
-        
-        if (array.length - index === 4) {
-            return Array(digit + 1).join("M");
+    return digits.map(function(digit, index, array) {
+        var powerOfTen = array.length - index - 1;
+        var powerOfTenValue = Math.pow(10, powerOfTen);
+
+        var currentPowerOfTenLiteral = romanNumeralsMap[powerOfTenValue];
+        var currentPowerOfTenFiveLiteral = romanNumeralsMap[5 * powerOfTenValue];
+
+        if (digit + 1 === 5){
+            return currentPowerOfTenLiteral + currentPowerOfTenFiveLiteral;
         }
 
-        if (array.length - index === 3) {
-            if(digit > 5){
-                return "D" + Array(digit - 5 + 1).join("C");
-            }
-            
-            return Array(digit + 1).join("C");
+        if( (digit + 1) * Math.pow(10, powerOfTen - 1)  ===  powerOfTenValue){
+            return currentPowerOfTenLiteral + romanNumeralsMap[powerOfTenValue * 10];
         }
 
-        if (array.length - index === 2) {
-            if(digit > 5){
-                return "L" + Array(digit - 5 + 1).join("X");
-            }            
-            
-            return Array(digit + 1).join("X");
+        if(digit > 5){
+            return currentPowerOfTenFiveLiteral + Array(digit - 5 + 1).join(currentPowerOfTenLiteral);
         }
 
-        if (array.length - index === 1) {
-            if(digit > 5){
-                return "V" + Array(digit - 5 + 1).join("I");
-            }            
-            
-            return Array(digit + 1).join("I");
-        }
+        return digit === 5 ? currentPowerOfTenFiveLiteral : Array(digit + 1).join(currentPowerOfTenLiteral);
     }).join("");
-
-    return decomposed;
 };
 
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line) {
