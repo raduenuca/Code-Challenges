dc5a89ef8b1fb39e4309187f60867b9143d328e0
Radu Enuca <raduenuca@gmail.com>
2015-08-19 11:29:45 +0000

    Updated the Major Element

--- Code Eval/Easy/JavaScript/theMajorElement.js
+++ Code Eval/Easy/JavaScript/theMajorElement.js
@@ -1,99 +1,173 @@
-var fs = require("fs");
+(function() {
+    var fs = require("fs");
 
-var curry = function (func) {
-    var initialArgs = [].slice.apply(arguments, [1]);
-    var funcArgsLength = func.length;
+    var curry = function(func) {
+        var initialArgs = [].slice.apply(arguments, [1]);
+        var funcArgsLength = func.length;
 
-    var curried = function(args) {
-        if (args.length >= funcArgsLength) {
-            return func.apply(null, args);
-        }
+        var curried = function(args) {
+            if (args.length >= funcArgsLength) {
+                return func.apply(null, args);
+            }
 
-        return function () {
-            return curried(args.concat([].slice.apply(arguments)));
+            return function() {
+                return curried(args.concat([].slice.apply(arguments)));
+            };
+        };
+
+        return curried(initialArgs);
+    };
+
+    var compose = function() {
+        var funcs = arguments;
+        return function() {
+            var args = arguments;
+            for (var i = funcs.length; i-- > 0;) {
+                args = [funcs[i].apply(this, args)];
+            }
+            return args[0];
         };
     };
 
-    return curried(initialArgs);
-};
+    var filter_ = function(fn, arr) {
+        var length = arr.length,
+            result = [],
+            i;
+        for (i = 0; i < length; i++) {
+            if (fn(arr[i], i, arr)) {
+                result.push(arr[i]);
+            }
+        }
+        return result;
+    };
 
-var joinArray = function(chr, array) {
-    return array.join(chr);
-};
+    var forEach_ = function(fn, arr) {
+        var length = arr.length,
+            i;
 
-var splitArray = function(chr, array) {
-    return array.split(chr);
-};
+        for (i = 0; i < length; i++) {
+            fn(arr[i], i, arr);
+        }
+    };
 
-var lines = curry(splitArray, "\n");
-var unlines = curry(joinArray, "\n");
+    var map_ = function(fn, arr) {
+        var length = arr.length,
+            result = new Array(length),
+            i;
 
-var csv = curry(splitArray, ",");
+        for (i = 0; i < length; i++) {
+            result[i] = fn(arr[i], i, arr);
+        }
+        return result;
+    };
 
-var map_ = function(fn, array){
-    return array.map(fn);
-};
+    var reduce_ = function(fn, accumulator, arr) {
+        var length = arr.length,
+            i, result;
 
-var reduce_ = function(fn, array, acc){
-    if (typeof acc === "undefined" || acc === null){
-        return array.reduce(fn);    
-    }
+        if (accumulator === undefined) {
+            i = 1;
+            result = arr[0];
+        }
+        else {
+            i = 0;
+            result = accumulator;
+        }
+
+        for (; i < length; i++) {
+            result = fn(result, arr[i], i, arr);
+        }
+
+        return result;
+    };
+
+    var joinArray = function(chr, array) {
+        return array.join(chr);
+    };
+
+    var splitArray = function(chr, array) {
+        return array.split(chr);
+    };
+
+    var show = function(value) {
+        console.log(value);
+    };
+
+    var getMaxOfArray = function(array) {
+        return Math.max.apply(null, array);
+    };
+
+    var generateArray = function(size, init) {
+        return Array.apply(null, {
+                length: size + 1
+            })
+            .map(function() {
+                return init;
+            });
+    };
+
+    var parseInt_ = function(value) {
+        return +value;
+    };
+
+    var distinct = function(acc, curr) {
+        if (!Array.isArray(acc)) {
+            var init = acc;
+            acc = generateArray(100, 0);
+            acc[init] = 1;
+        }
+
+        var currentElement = acc[curr];
+        if (typeof currentElement !== "undefined") {
+            currentElement = currentElement + 1;
+        }
+        else {
+            currentElement = 1;
+        }
+
+        acc[curr] = currentElement;
+        return acc;
+    };
+
+    var lines = curry(splitArray, "\n");
+    var unlines = curry(joinArray, "\n");
+
+    var words = curry(splitArray, " ");
+    var concat = curry(joinArray, "");
+
+    var csv = curry(splitArray, ",");
+
+    var readCsv = curry(map_, csv);
+    var convertToNumberArray = curry(map_, parseInt_);
+    var countAppearances = curry(reduce_, distinct, undefined);
+
+    var sum = function(a, b) {
+        return a + b;
+    };
     
-    return array.reduce(fn, acc);
-};
+    var nonEmpty = curry(filter_, Boolean);
 
-var parseInt_ = function(value){
-    return +value;
-};
+    var computeMajorElements = function(array) {
+        var maxElement = getMaxOfArray(array);
+        var sequenceLength = reduce_(sum, undefined, array);
+        if (maxElement >= sequenceLength / 2) {
+            return array.indexOf(maxElement);
+        }
 
-var generateArray = function(size, init){
-    return Array.apply(null, {length: size + 1})
-        .map(function() { return init;});
-};
+        return "None";
+    };
 
-var getMaxOfArray = function(array) {
-    return Math.max.apply(null, array);
-};
+    var computeMajorElements_ = curry(map_, computeMajorElements);
+    var countAppearances_ = curry(map_, countAppearances);
+    var convertToNumberArray_ = curry(map_, convertToNumberArray);
 
-var convertToNumberArray = curry(map_, parseInt_);
+    var computeMajorElement = compose(
+        computeMajorElements_,
+        countAppearances_,
+        convertToNumberArray_,
+        readCsv);
 
-var distinct = function(acc, curr) {
-    if (!Array.isArray(acc)){
-        acc = generateArray(100, 0);
-    }
-    
-    var currentElement = acc[curr];
-    if (typeof currentElement !== "undefined") {
-        currentElement = currentElement + 1;
-    }
-    else {
-        currentElement = 1;
-    }
+    var majorElements = compose(show, unlines, computeMajorElement, nonEmpty, lines);
 
-    acc[curr] = currentElement;
-    return acc;
-};
-
-var sum = function(a, b){
-    return a + b;
-};
-
-var appearances = curry(reduce_, distinct);
-
-var computeMajorElements = function(array){
-    var maxElement = getMaxOfArray(array);
-    var sequenceLength = array.reduce(sum);
-    if( maxElement >= sequenceLength / 2){
-        return array.indexOf(maxElement);
-    }
-    
-    return "None";
-};
-
-var majorElements = lines(fs.readFileSync(process.argv[2]).toString())
-    .map(csv)
-    .map(convertToNumberArray)
-    .map(appearances)
-    .map(computeMajorElements);
-
-console.log(unlines(majorElements));
+    majorElements(fs.readFileSync(process.argv[2]).toString());
+}());
